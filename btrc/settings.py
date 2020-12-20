29ae7fc0840cf3a054655d3c311fef9f2d8542fc
Kazi Shafin <kazishafin1@gmail.com>
2020-12-20 21:10:19 +0600

    DEBUG RESET

--- btrc/settings.py
+++ btrc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fdy^j@3(n4tk%o04&z1z1n(*ce0imywk(tdpveui)6xdoj&@gw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kazirealstate.herokuapp.com', '127.0.0.1']
 

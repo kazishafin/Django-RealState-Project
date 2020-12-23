8f87360e3ffe2898af1d8628f1ab98022544d930
Kazi Shafin <kazishafin1@gmail.com>
2020-12-23 14:07:19 +0600

    ..

--- btrc/urls.py
+++ btrc/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
     
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
